fe980dac86af9a55b2a2401512138caa69a081cc
Ozcan008 <brad_tyler@fastmail.fm>
2016-03-07 13:52:40 -0700

    Added files via upload

---
+++ HelloWorld/src/quickstart/HelloWorld.java
@@ -0,0 +1,10 @@
+package quickstart;
+
+/**
+ * Created by brad on 06/03/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
